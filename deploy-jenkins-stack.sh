eeaeb91a8183b77a334d77050b1dd9a504598b31
katson95 <katson95@gmail.com>
2018-09-19 23:06:40 +0200

    Update deploy-jenkins-stack.sh

--- deploy-jenkins-stack.sh
+++ deploy-jenkins-stack.sh
@@ -2,13 +2,13 @@
 
 DATE=$(date -I)
 
-docker build . --no-cache -t jamessmith52963/jenkins:$DATE
-docker tag jamessmith52963/jenkins:$DATE jamessmith52963/jenkins:lts 
-docker tag jamessmith52963/jenkins:$DATE jamessmith52963/jenkins:latest
+docker build . --no-cache -t katson95/jenkins:$DATE
+docker tag katson95/jenkins:$DATE katson95/jenkins:lts 
+docker tag katson95/jenkins:$DATE katson95/jenkins:latest
 
-docker push jamessmith52963/jenkins:$DATE
-docker push jamessmith52963/jenkins:latest
-docker push jamessmith52963/jenkins:lts
+docker push katson95/jenkins:$DATE
+docker push katson95/jenkins:latest
+docker push katson95/jenkins:lts
 
 docker stack rm jenkins
 sleep 15
